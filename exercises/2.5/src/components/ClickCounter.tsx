a45a57766485ed77b83b3a2eccf58993290cd392
alireda <alireda.touzani@student.vinci.be>
2024-11-02 14:06:31 +0100

    new:ex2.5-destructuring

--- exercises/2.5/src/components/ClickCounter.tsx
+++ exercises/2.5/src/components/ClickCounter.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 
-const ClickCounter = () => {
+interface ClickCounterProps {
+  title: string;
+  message?: string;
+}
+
+const ClickCounter = ({ title, message }: ClickCounterProps) => {
   const [count, setCount] = useState(0);
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+    <div>
+      <h2>{title}</h2>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      {count >= 10 ? <p>{message}</p> : null}
+    </div>
   );
 };
 
