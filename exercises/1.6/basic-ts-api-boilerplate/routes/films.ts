4e135509e752c37f114fd0d1d6e824bd88af75f7
alireda <alireda.touzani@student.vinci.be>
2024-11-02 19:10:00 +0100

    new:ex1.6

--- exercises/1.6/basic-ts-api-boilerplate/routes/films.ts
+++ exercises/1.6/basic-ts-api-boilerplate/routes/films.ts
@@ -35,7 +35,7 @@ router.get("/", (req, res) => {
   if (!req.query["minimum-duration"]) return res.json(defaultFilms);
 
   const minimumDuration = Number(req.query["minimum-duration"]);
-  if (minimumDuration <= 0 || typeof minimumDuration !== "number")
+  if (minimumDuration <= 0 || isNaN(minimumDuration))
     return res.sendStatus(400);
 
   const filteredFilms = defaultFilms.filter((film) => {
@@ -47,6 +47,8 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
+  if (isNaN(id)) return res.sendStatus(400);
+
   const film = defaultFilms.find((film) => film.id === id);
   if (!film) return res.sendStatus(404);
 
@@ -65,46 +67,44 @@ router.post("/", (req, res) => {
     typeof body.director !== "string" ||
     typeof body.duration !== "number" ||
     !body.title.trim() ||
+    !body.director.trim() ||
     body.duration <= 0 ||
-    ("budget" in body && typeof body.budget !== "number") ||
-    ("budget" in body && typeof body.budget === "number" && body.budget <= 0) ||
-    ("description" in body && typeof body.description !== "string") ||
-    ("imageUrl" in body && typeof body.imageUrl !== "string")
+    ("budget" in body &&
+      (typeof body.budget !== "number" || body.budget <= 0)) ||
+    ("description" in body &&
+      (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body &&
+      (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
   ) {
     return res.sendStatus(400);
   }
 
-  for (let index = 0; index < defaultFilms.length; index++) {
-    if (body.title === defaultFilms[index].title && body.director === defaultFilms[index].director) {
-      return res.sendStatus(409);
-    }
+  const newFilm = body as NewFilm;
+
+  const existingFilm = defaultFilms.find(
+    (film) =>
+      film.title.toLowerCase() === newFilm.title.toLowerCase() &&
+      film.director.toLowerCase() === newFilm.director.toLowerCase()
+  );
+
+  if (existingFilm) {
+    return res.sendStatus(409);
   }
 
-  const { title, director, duration, budget, description, imageUrl } =
-    body as NewFilm;
-
   const nextId =
-    defaultFilms.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
+    defaultFilms.reduce((acc, film) => (film.id > acc ? film.id : acc), 0) + 1;
 
-  const newDrink: Film = {
-    id: nextId,
-    title,
-    director,
-    duration,
-    budget,
-    description,
-    imageUrl
-  };
+  const addedFilm: Film = { id: nextId, ...newFilm };
 
-  defaultFilms.push(newDrink);
-  return res.json(newDrink);
+  defaultFilms.push(addedFilm);
+  return res.json(addedFilm);
 });
-
-
-
 
 router.delete("/:id", (req, res) => {
   const id = Number(req.params.id);
+  if (isNaN(id)) {
+    return res.sendStatus(400);
+  }
   const filmId = defaultFilms.findIndex((film) => film.id === id);
   if (filmId === -1) {
     return res.sendStatus(404);
@@ -112,64 +112,48 @@ router.delete("/:id", (req, res) => {
 
   const deletedFilms = defaultFilms.splice(filmId, 1);
 
-  return res.json(deletedFilms[0]);
+  return res.json(deletedFilms);
 });
 
 router.patch("/:id", (req, res) => {
   const id = Number(req.params.id);
+  if (isNaN(id)) {
+    return res.sendStatus(400);
+  }
   const film = defaultFilms.find((film) => film.id === id);
   if (!film) {
     return res.sendStatus(404);
   }
 
-  const body : unknown = req.body;
+  const body: unknown = req.body;
   if (
     !body ||
     typeof body !== "object" ||
+    Object.keys(body).length === 0 ||
     ("title" in body &&
       (typeof body.title !== "string" || !body.title.trim())) ||
-    ("imageUrl" in body &&
-      (typeof body.imageUrl !== "string" || !body.imageUrl.trim())) ||
     ("director" in body &&
       (typeof body.director !== "string" || !body.director.trim())) ||
-    ("duration" in body && (typeof body.duration !== "number" || body.duration <= 0)) ||
-    ("budget" in body && (typeof body.budget !== "number" || body.budget <= 0)) ||
-    ("description" in body && (typeof body.description !== "string" || !body.description.trim()))
+    ("duration" in body &&
+      (typeof body.duration !== "number" || body.duration <= 0)) ||
+    ("budget" in body &&
+      (typeof body.budget !== "number" || body.budget <= 0)) ||
+    ("description" in body &&
+      (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body &&
+      (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
   ) {
     return res.sendStatus(400);
   }
-  const {title, director, duration, budget, description, imageUrl} : Partial<NewFilm> = body;
-    
-    if(title){
-      film.title = title;
-    }
 
-    if(director){
-      film.director = director;
-    }
+  const updatedFilm = { ...film, ...body };
+  defaultFilms[defaultFilms.indexOf(film)] = updatedFilm;
 
-    if (duration) {
-      film.duration = duration;
-    }
-
-    if (budget) {
-      film.budget = budget;
-    }
-
-    if (description) {
-      film.description = description;
-    }
-
-    if (imageUrl) {
-      film.imageUrl = imageUrl;
-    }
-
-    return res.json(film);
+  return res.json(film);
 });
 
-
 router.put("/:id", (req, res) => {
-  const body : unknown = req.body;
+  const body: unknown = req.body;
 
   if (
     !body ||
@@ -203,14 +187,13 @@ router.put("/:id", (req, res) => {
 
   if (indexOfFilmToUpdate < 0) {
     const newFilm = body as NewFilm;
-    const nextId =
-      defaultFilms.reduce((acc, film) => (film.id > acc ? film.id : acc), 0) + 1;
+      const nextId = defaultFilms.reduce((acc, film) => (film.id > acc ? film.id : acc), 0) +1;
 
-    const addedFilm = { id: nextId, ...newFilm };
+      const addedFilm = { id: nextId, ...newFilm };
 
-    defaultFilms.push(addedFilm);
+      defaultFilms.push(addedFilm);
 
-    return res.json(addedFilm);
+      return res.json(addedFilm);
   }
 
   const updatedFilm = { ...defaultFilms[indexOfFilmToUpdate], ...body } as Film;
