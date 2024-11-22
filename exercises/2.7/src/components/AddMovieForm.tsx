c87863a2d18ef1cd421e1a16e146cfbd9db5b07c
Ali-Reda Touzani <alireda.touzani@student.vinci.be>
2024-11-22 11:46:30 +0100

    new:ex2.7

--- exercises/2.7/src/components/AddMovieForm.tsx
+++ exercises/2.7/src/components/AddMovieForm.tsx
@@ -2,28 +2,95 @@ import { useState, SyntheticEvent } from "react";
 import { Movie } from "../types";
 
 interface AddMovieFormProps {
-    onMovieAdded: (movie: Movie) => void;
+    onAddMovie: (movie: Movie) => void;
 }
 
-const AddMovieForm = ({onMovieAdded}: AddMovieFormProps) => {
-    const [title, setTitle] = useState("");
-    const [director, setDirector] = useState("");
-    const [duration, setDuration] = useState(0);
-    const [newPicture, setPicture] = useState("");
-    const [newDescription, setDescription] = useState("");
-    const [newBudget, setBudget] = useState(0);
+const AddMovieForm = ({onAddMovie}: AddMovieFormProps) => {
+  const [newTitle, setTitle] = useState("");
+  const [newDirector, setDirector] = useState("");
+  const [newDuration, setDuration] = useState(0);
+  const [newPicture, setPicture] = useState("");
+  const [newDescription, setDescription] = useState("");
+  const [newBudget, setBudget] = useState(0);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    const handleSubmit = (e: SyntheticEvent) => {
-        e.preventDefault();
-        console.log("Submit: ", newTitle, newDirector, newDuration, newPicture, newDescription, newBudget);
-        onMovieAdded({title, director, duration, pictureUrl, newDescription, newBudget});
-        setTitle("");
-        setDirector("");
-        setDuration(0);
-        setPicture("");
-        s
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(
+      "Submit: ",
+      newTitle,
+      newDirector,
+      newDuration,
+      newPicture,
+      newDescription,
+      newBudget
+    );
+
+    const newMovie: Movie = {
+      title: newTitle,
+      director: newDirector,
+      duration: newDuration,
+      pictureUrl: newPicture,
+      description: newDescription,
+      budget: newBudget,
     };
-    return (
 
-    )
-}
+    onAddMovie(newMovie);
+    setMovies([...movies, newMovie]);
+
+    console.log("New movie: ", newMovie);
+    setTitle("");
+    setDirector("");
+    setDuration(0);
+    setPicture("");
+    setDescription("");
+    setBudget(0);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={newTitle}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Director"
+        value={newDirector}
+        onChange={(e) => setDirector(e.target.value)}
+        required
+      />
+      <input
+        type="number"
+        placeholder="Duration"
+        value={newDuration}
+        onChange={(e) => setDuration(parseInt(e.target.value))}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Picture"
+        value={newPicture}
+        onChange={(e) => setPicture(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        value={newDescription}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Budget"
+        value={newBudget}
+        onChange={(e) => setBudget(parseInt(e.target.value))}
+      />
+      
+      <button type="submit">Ajouter</button>
+    </form>
+  );
+};
+
+export default AddMovieForm;
